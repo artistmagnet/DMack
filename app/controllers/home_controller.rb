f73d4b13b6db2b2e7379a1f0061d214d790c37c6
Giorgio Sidari <giorgio.sidari@fastwebnet.it>
2014-10-08 11:47:24 +0200

    Random welcome message

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,10 @@
 class HomeController < ApplicationController
 
   def index
+    greetings = ["Hello", "Welcome back", "Hi", "Hey"]
+    number = DateTime.now.to_time.to_i
+    index = Random.new(number).rand(greetings.size)
+    @greeting = greetings[index]
   end
 
 end
