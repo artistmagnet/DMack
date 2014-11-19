049b215bfb9275b73fa4d5d82dd5225ef5c4f07d
Giorgio Sidari <giorgio.sidari@fastwebnet.it>
2014-11-19 19:20:38 +0100

    Less production functions presented to normal users

---
+++ spec/features/production_feature_spec.rb
@@ -0,0 +1,13 @@
+require 'rails_helper'
+require 'spec_lib'
+
+feature "Production" do
+  before :each do
+  end
+
+#TODO
+  # scenario "should not be editable by generic users", :js => true do
+  # end
+
+end
+
