8247b36931c1354fe293aa933b1e0fd1ff061629
Giorgio Sidari <giorgio.sidari@fastwebnet.it>
2014-12-09 16:22:52 +0100

    FEATURE Credit creation should allow to change director after production has been selected

--- spec/features/credit_feature_spec.rb
+++ spec/features/credit_feature_spec.rb
@@ -3,7 +3,6 @@ require 'spec_lib'
 
 feature "Credit" do
   before :each do
-    user ||= FactoryGirl.create(:user)
     visit_home
   end
 
@@ -11,9 +10,17 @@ feature "Credit" do
     scenario "should not allow to change director if production is unselected", :js => true do
       click_on 'New Resume'
       click_link 'Add Credit'
-      # save_and_open_page
       director_field = find("#role_director_id_chosen")
       expect(director_field).to have_css('[disabled]')
+    end
+
+    scenario "should allow to change director after production has been selected", :js => true do
+      click_on 'New Resume'
+      click_link 'Add Credit'
+      select_from_chosen 'Test Production', :from => 'Production*'
+      # save_and_open_page
+      director_field = find("#role_director_id_chosen")
+      expect(director_field).not_to have_css('[disabled]')
     end
 
   end
--- spec/spec_lib.rb
+++ spec/spec_lib.rb
@@ -1,3 +1,58 @@
 def visit_home(locale = I18n.default_locale)
   visit "/"
 end
+
+# def select_from_chosen(item_text, options)
+#   puts "ITEM: #{item_text}, OPTIONS: #{options}"
+#   field = find_field(options[:from], visible: false)
+#   option_value = page.evaluate_script("$(\"##{field[:id]} option:contains('#{item_text}')\").val()")
+#   page.execute_script("value = ['#{option_value}']\; if ($('##{field[:id]}').val()) {$.merge(value, $('##{field[:id]}').val())}")
+#   option_value = page.evaluate_script("value")
+#   page.execute_script("$('##{field[:id]}').val(#{option_value})")
+#   page.execute_script("$('##{field[:id]}').trigger('chosen:updated')")
+# end
+
+
+def select_from_chosen(item_text, options)
+  # Source: https://gist.github.com/michael-harrison/4102026
+  # =begin
+  # Notes
+  # =====
+  # Labels: On the label you should put a "for" attribute if you're not using something like simple_form
+  # This helps capybara to find your field
+  # e.g. <label for="my_field_id">Some label</label>
+  # =end
+
+  field = options[:from]
+  value = item_text
+
+
+  field_name = options[:from]
+  selected_value = item_text
+
+  # Find the field
+  field = find_field(field_name, visible: false)
+  # Open the Chosen drop down
+  page.execute_script(%Q!$("##{field[:id]}_chosen").mousedown()!)
+  # Simulate text being typed
+  typed_text = ""
+  value.chars.each do |char|
+    typed_text += char
+  # Put a value in the search field
+    page.execute_script(%Q!$("##{field[:id]}_chosen input").val("#{typed_text}")!)
+  # Fire the search via a keyup
+    page.execute_script(%Q!$("##{field[:id]}_chosen input").keyup()!)
+  end
+
+  # If you're using Ajax Chosen then wait for the Ajax callback
+  sleep 1
+  start_time = Time.now
+  page.evaluate_script('jQuery.isReady&&jQuery.active==0').class.should_not eql(String) until page.evaluate_script('jQuery.isReady&&jQuery.active==0') or (start_time + 5.seconds) < Time.now do
+    sleep 1
+  end
+
+  # Do the selection via firing a mouse up on the option
+  page.execute_script(%Q!$("##{field[:id]}_chosen .chosen-results li:contains('#{value}')").mouseup()!)
+end
+
+
