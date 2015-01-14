52d9610ad3916db8fdeb9115885f5f51afaca45c
Giorgio Sidari <giorgio@sidari.it>
2015-01-14 11:48:08 +0100

    Showing rtable lines on the resume page

--- app/models/section_slot.rb
+++ app/models/section_slot.rb
@@ -1,4 +1,8 @@
 class SectionSlot < ActiveRecord::Base
   belongs_to :section, :polymorphic => true
   belongs_to :resume
+
+  def self.edit_section_path(params)
+    edit_education_table_path(params)
+  end
 end
