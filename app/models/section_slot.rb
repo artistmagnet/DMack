c9796db7d191980ad2f269c3985b1dac5aa7f4ec
Giorgio Sidari <giorgio@sidari.it>
2015-01-14 12:28:36 +0100

    Polymorphic path

--- app/models/section_slot.rb
+++ app/models/section_slot.rb
@@ -2,7 +2,4 @@ class SectionSlot < ActiveRecord::Base
   belongs_to :section, :polymorphic => true
   belongs_to :resume
 
-  def self.edit_section_path(params)
-    edit_education_table_path(params)
-  end
 end
