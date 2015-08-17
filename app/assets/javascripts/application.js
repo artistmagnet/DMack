7b555e27a65357a5ce05f1fe72ddb1243d790383
manishnagdewani96170 <nagdewanimanis@gmail.com>
2015-08-17 22:15:50 +0530

    added new changes

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -47,39 +47,7 @@ $(document).ready(function(){
   $('.alert .close').on('click', function(e) {
       $(this).parent().hide();
   });
-  $(function () {
-	  window.NestedFormEvents.prototype.insertFields = function(content, assoc, link) {
-	  var $tr = $(link).closest('table').find('tbody tr:last');
-	  var new_id = new Date().getTime();
-	  var new_tr = $($tr).clone();
-	  
-	  new_tr.find('td').each(function(i,el){
-        var tr_input = $(el).find('input');
-        tr_input.attr('placeholder','').val('');
-        var id_val = tr_input.attr('id');
-        if(i<6){
-	        
-	        if(id_val=="1"||id_val=="2"||id_val=="3"||id_val=="4"||id_val=="5"||id_val=="6"){
-	        	var name = tr_input.attr('name');
-	        	var new_name = "resume[educations_attributes]["+new_id+"]["+name+"]";
-	        	tr_input.attr('name',new_name);
-	        	tr_input.attr('id',"resume_educations_attributes_"+new_id+"_"+name);
-	        	
-	        }
-	        else{
-		        var id_arr = id_val.split("_");
-		        id_arr[3] = new_id; 
-		        var new_name = id_arr[0]+"["+id_arr[1]+"_"+id_arr[2]+"]"+"["+id_arr[3]+"]"+"["+id_arr[4]+"]";
-		        tr_input.attr('name',new_name);
-		        $(el).find('input').attr('id',id_arr.join("_"));
-		        
-	    	} 
-	    } 
-	  });
-	  new_tr = "<tr class='fields'>"+new_tr.html()+"</tr>";
-	  return $(new_tr).insertAfter($tr);
-   }
-  });
+  
 });
 // function tags(){
 // 	$('.tag-input').tagsinput('add', 'some tag');
