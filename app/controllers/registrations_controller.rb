386be239246dd55bacd9e4c31ad986c35d342542
phagoth <olexandr.averyanov@gmail.com>
2016-03-31 00:15:02 +0300

    ReCAPTCHA installed

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,12 +1,13 @@
 class RegistrationsController < Devise::RegistrationsController
-
+  prepend_before_action :check_captcha, only: [:create]
+  
   def new
     super
   end
 
   def create
     @user= User.new(params[:user].permit!)
-    if simple_captcha_valid?     
+    # if simple_captcha_valid?     
       respond_to do |format|
         if @user.save
           #UserMailer.welcome_email(@user, params[:user][:password]).deliver
@@ -17,10 +18,10 @@ class RegistrationsController < Devise::RegistrationsController
           format.json { render json: @user.errors, status: :unprocessable_entity }
         end
       end
-    else      
-      flash[:error] = "There was an error with the captcha code below. Please re-enter the code and click Sign Up."
-      render :new
-    end
+    # else      
+    #   flash[:error] = "There was an error with the captcha code below. Please re-enter the code and click Sign Up."
+    #   render :new
+    # end
   end
 
   def update
@@ -30,6 +31,16 @@ class RegistrationsController < Devise::RegistrationsController
   protected
     def update_resource(resource, params)
       resource.update_without_password(params)
+    end
+  
+  private
+    def check_captcha
+      if verify_recaptcha
+        true
+      else
+        self.resource = resource_class.new sign_up_params
+        respond_with_navigational(resource) { render :new }
+      end 
     end
  
 end 
