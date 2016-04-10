04c958e90d89992a10cbf07256e1944296ecd313
manish n <nagdewanimanis@gmail.com>
2016-04-10 20:50:57 +0530

    changes in wicked pdf

--- config/initializers/wicked_pdf.rb
+++ config/initializers/wicked_pdf.rb
@@ -4,10 +4,10 @@
 #   :exe_path => '/usr/bin/wkhtmltopdf'
 # }
 WickedPdf.config do |config|  
-  if Rails.env == 'production' then
-    config.exe_path = Rails.root.to_s + "/bin/wkhtmltopdf"
-  else
-    # config.exe_path = '/usr/bin/wkhtmltopdf'
-    config.exe_path = Rails.root.to_s + "/bin/wkhtmltopdf"
-  end
+  # if Rails.env == 'production' then
+  #   config.exe_path = Rails.root.to_s + "/bin/wkhtmltopdf"
+  # else
+  #   # config.exe_path = '/usr/bin/wkhtmltopdf'
+  #   config.exe_path = Rails.root.to_s + "/bin/wkhtmltopdf"
+  # end
 end
