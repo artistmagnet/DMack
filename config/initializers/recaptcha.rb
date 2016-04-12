35276be1d0d6c2021dff4527079103056d8befdf
phagoth <olexandr.averyanov@gmail.com>
2016-04-13 01:26:19 +0300

    set temp recaptcha keys

---
+++ config/initializers/recaptcha.rb
@@ -0,0 +1,6 @@
+Recaptcha.configure do |config|
+  config.public_key  = '6LfAIhwTAAAAAGBNHIdRpc2MR3VvyNvdBJM02y13'
+  config.private_key = '6LfAIhwTAAAAAIKref4WDKT6EgIWzT8BRiTXURF0'
+  # Uncomment the following line if you are using a proxy server:
+  # config.proxy = 'http://myproxy.com.au:8080'
+end
